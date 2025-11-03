14e4139f969b4ee647281e452a62cbadecd0c445
王壮 <wangzhuang13@huawei.com>
2025-11-03 20:34:10 +0800

    需求编码：【openEuler 24.03 LTS SP3】TSB-agent: 支持可信计算虚拟化vTPCM https://gitee.com/openeuler/release-management/issues/ID3BZ8
    修改描述：完成libguestfs模块的UT框架搭建，验证代码质量

---
+++ virtrust/src/virtrust/dllib/libguestfs_test.cpp
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) Huawei Technologies Co., Ltd. 2025-2025.All rights reserved.
+*/
+
+#include "gtest/gtest.h"
+
+#include "virtrust/dllib/libguestfs.h"
+
+namespace virtrust {
+    
+    TEST(LibGuestfsTest, works)
+    {
+        // Init
+        auto& libguestfs = Libguestfs::GetInstance();
+        EXPECT_EQ(libguestfs.CheckOK(), DllibRc::OK);
+
+        // Explicity reload
+        auto ret = libguestfs.Reload();
+        EXPECT_EQ(ret, DllibRc::OK);
+
+        // Call Function
+        auto *g = libguestfs.guestfs_create();
+        EXPECT_NE(g, nullptr);
+
+        // quit
+        if (g != nullptr) {
+            libguestfs.guestfs_close(g);
+        }
+    }
+        
+    TEST(DynamicLibraryTest, SingletonPattern)
+    {
+        // Test that libguestfs follow singleton pattern
+        auto& libguestfs1 = Libguestfs::GetInstance();
+        auto& libguestfs2 = Libguestfs::GetInstance();
+        EXPECT_EQ(&libguestfs1, &libguestfs2);
+    }
+    
+    TEST(DynamicLibraryTest, CheckOKFunction)
+    {
+        // Test the CheckOK function
+        auto& libguestfs = Libguestfs::GetInstance();
+        EXPECT_EQ(libguestfs.CheckOK(), DllibRc::OK);
+
+        // Test size function
+        EXPECT_GT(libguestfs.size(), (size_t)0);
+    }
+    
+    TEST(DynamicLibraryTest, ReloadFunction)
+    {
+        // Test reloading functionality
+        auto& libguestfs = Libguestfs::GetInstance();
+
+        // check inital state
+        EXPECT_EQ(libguestfs.CheckOK(), DllibRc::OK);
+
+        // Reload the library
+        auto ret = libguestfs.Reload();
+        EXPECT_EQ(ret, DllibRc::OK);
+
+        // Verify it's still working after reload
+        EXPECT_EQ(libguestfs.CheckOK(), DllibRc::OK);
+    }
+    
+    TEST(DynamicLibraryTest, FunctionPointerValidity)
+    {
+        // Test that all function pointer are valid
+        auto& libguestfs = Libguestfs::GetInstance();
+
+        // Test a few key functions for validity
+        EXPECT_NE(libguestfs.guestfs_create.Get(), nullptr);
+        EXPECT_NE(libguestfs.guestfs_launch.Get(), nullptr);
+        EXPECT_NE(libguestfs.guestfs_close.Get(), nullptr);
+
+        // Test that functions can be called (without actaully executing)
+        EXPECT_FALSE(libguestfs.guestfs_create.GetName());
+        EXPECT_FALSE(libguestfs.guestfs_launch.GetName());
+        EXPECT_FALSE(libguestfs.guestfs_close.GetName());
+    }
+} // namespace virtrust
