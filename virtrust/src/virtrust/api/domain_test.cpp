5e6aa7d5bc2d748ae4646441f0e01622e97cfe55
王壮 <wangzhuang13@huawei.com>
2025-11-03 10:42:23 +0800

    需求编码：【openEuler 24.03 LTS SP3】TSB-agent: 支持可信计算虚拟化vTPCM https://gitee.com/openeuler/release-management/issues/ID3BZ8
    修改描述：完成api模块的UT框架搭建，验证代码质量

---
+++ virtrust/src/virtrust/api/domain_test.cpp
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) Huawei Technologies Co., Ltd. 2025-2025.All rights reserved.
+*/
+
+#include <gtest/gtest.h>
+#include <string>
+#include <vector>
+#include <memory>
+
+#include "virtrust/api/domain.h"
+#include "virtrust/api/context.h"
+#include "virtrust/api/defines.h"
+#include "virtrust/utils/foreign_mounter.h"
+#include "virtrust/utils/virt_xml_parser.h"
+#include "virtrust/utils/file_io.h"
+#include "virtrust/crypto/sm3.h"
+#include "virtrust/dllib/libvirt.h"
+#include "virtrust/base/logger.h"
+
+namespace virtrust {
+    
+    // Test that basic compilation works
+    TEST(DomainTest, BasicCompilation) {
+        // Basic compile-time test - just ensure the functions exist and can be called
+        EXPECT_TRUE(true);
+    }
+    
+    // Test that VerifyConfig can be instantiated and used
+    TEST(DomainTest, VerifyConfigBasics) {
+        // Test that VerifyConfig can be constructed with basic parameters
+        VerifyConfig config("test-guest", "/path/to/disk", "/path/to/loader");
+
+        // Test basic getter functions exist and work
+        EXPECT_EQ(config.GetGuestName(), "test-guest");
+        EXPECT_EQ(config.GetDiskPath(), "/path/to/disk");
+        EXPECT_EQ(config.GetLoaderPath(), "/path/to/loader");
+    }
+} // namespace virtrust
