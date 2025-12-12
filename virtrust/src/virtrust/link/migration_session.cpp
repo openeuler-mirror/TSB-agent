b96d94deb525521b947d53a1fd170bafde81dbd2
guomangmang <guomangmang1@h-partners.com>
2025-12-12 11:53:42 +0800

    修改日志问题

--- virtrust/src/virtrust/link/migration_session.cpp
+++ virtrust/src/virtrust/link/migration_session.cpp
@@ -840,6 +840,7 @@ MigrateSessionRc MigrationSession::OnFinishedRequestReceived(bool finished)
         Cleanup();
         return MigrateSessionRc::OK;
     }
+    VIRTRUST_LOG_DEBUG("|OnFinishedRequestReceived|END|returnS|domain name: {}|Migrate success.", domainName_);
 
     // 通知TSB迁移成功，如果调用失败，TSB虚机状态会保持 VM_MIGRATION
     auto rc = NotifyVRMigration(true);
@@ -852,7 +853,6 @@ MigrateSessionRc MigrationSession::OnFinishedRequestReceived(bool finished)
 
     EnterState(State::Finished);
     Cleanup();
-    VIRTRUST_LOG_DEBUG("|OnFinishedRequestReceived|END|returnS|domain name: {}|Migrate success.", domainName_);
     return MigrateSessionRc::OK;
 }
 } // namespace virtrust
